394d26c19876f1d6aca67ae257687ace0332bcd1
Felipe Lozano <david.loza010@gmail.com>
2023-08-10 15:10:34 -0400

    added routes for -view members- button

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsListVue from './components/teams/TeamsList.vue';
 import UsersListVue from './components/users/UsersList.vue';
-
+import TeamMemberVue from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
     // this adds a routing history, (like what the last visited page was)
@@ -11,7 +11,8 @@ const router = createRouter({
     // this is where we define the routes
     routes: [
         {path: '/teams', component: TeamsListVue}, // our-domain.com/teams => TeamsListVue
-        {path: '/users', component: UsersListVue}
+        {path: '/users', component: UsersListVue},
+        {path: '/teams/:userId', component: TeamMemberVue}
     ]
 });
 
